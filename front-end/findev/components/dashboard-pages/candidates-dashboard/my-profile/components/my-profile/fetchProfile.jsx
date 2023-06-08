28288f6f4c7006a186c79374109a995c7a51ac1b
Eakan <58219087+Eakan-Git@users.noreply.github.com>
2023-06-09 02:26:43 +0700

    Update fetchProfile.jsx

--- front-end/findev/components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/fetchProfile.jsx
+++ front-end/findev/components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/fetchProfile.jsx
@@ -11,13 +11,14 @@ export const fetchProfile = async (id, token) => {
     try {
       const response = await fetch(url, { method: 'GET', headers });
       if (!response.ok) {
-        throw new Error('Failed to fetch profile data');
+        // throw new Error('Failed to fetch profile data');
+        console.error('Phiên làm việc đã hết hạn, vui lòng đăng nhập lại');
       }
   
       const data = await response.json();
       return data;
     } catch (error) {
       console.error(error);
-      throw error;
+      // throw error;
     }
   };
