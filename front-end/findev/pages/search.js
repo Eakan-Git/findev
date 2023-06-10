038fc2783a09a031acb098d247a6deb0a3cfe86d
Eakan <58219087+Eakan-Git@users.noreply.github.com>
2023-06-10 23:52:44 +0700

    update search

---
+++ front-end/findev/pages/search.js
@@ -0,0 +1,14 @@
+import dynamic from "next/dynamic";
+import Seo from "../components/common/Seo";
+import FindJobs from "../components/job-listing-pages/search-list";
+
+const index = () => {
+  return (
+    <>
+      <Seo pageTitle="Tìm kiếm" />
+      <FindJobs />
+    </>
+  );
+};
+
+export default dynamic(() => Promise.resolve(index), { ssr: false });
