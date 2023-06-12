2d921cd8c2c05dcbdbdfd262b3006d85a7a1984f
nhut19112001 <nhutsadow@gmail.com>
2023-06-12 17:06:00 +0700

    Update Form.jsx

--- front-end/findev/components/dashboard-pages/candidates-dashboard/change-password/components/Form.jsx
+++ front-end/findev/components/dashboard-pages/candidates-dashboard/change-password/components/Form.jsx
@@ -1,44 +1,65 @@
+import axios from 'axios';
+import { localUrl } from "../../../../../utils/path.js";
+import React, { useState } from 'react';
 
-const Form = ({user}) => {
-  const handleChangePassword = (e) => {
+const Form = ({ user }) => {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+
+  const handleChangePassword = async (e) => {
     e.preventDefault();
-    const { current_password, new_password, confirm_new_password } = e.target;
-    if (new_password.value !== confirm_new_password.value) {
+
+    if (newPassword !== confirmNewPassword) {
       alert("Mật khẩu mới không khớp");
       return;
     }
-    const data = {
-      current_password: current_password.value,
-      new_password: new_password.value,
-    };
-    console.log(data);
+
+    try {
+      await axios.put(
+        `${localUrl}/user/password`,
+        {
+          current_password: currentPassword,
+          new_password: newPassword,
+          confirm_password: confirmNewPassword
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: user.token,
+          },
+        }
+      );
+      alert("Bạn đã thay đổi mật khẩu thành công");
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <form className="default-form">
       <div className="row">
         {/* <!-- Input --> */}
         <div className="form-group col-lg-7 col-md-12">
           <label>Mật khẩu hiện tại</label>
-          <input type="password" name="current_password" required />
+          <input type="password" name="current_password" required value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} />
         </div>
 
         {/* <!-- Input --> */}
         <div className="form-group col-lg-7 col-md-12">
           <label>Mật khẩu mới</label>
-          <input type="password" name="new_password" required />
+          <input type="password" name="new_password" required value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
         </div>
 
         {/* <!-- Input --> */}
         <div className="form-group col-lg-7 col-md-12">
           <label>Nhập lại mật khẩu mới</label>
-          <input type="password" name="confirm_new_password" required />
+          <input type="password" name="confirm_new_password" required value={confirmNewPassword} onChange={(e) => setConfirmNewPassword(e.target.value)} />
         </div>
 
         {/* <!-- Input --> */}
         <div className="form-group col-lg-6 col-md-12">
-          <button type="submit" className="theme-btn btn-style-one"
-            onClick={handleChangePassword}
-          >
+          <button type="submit" className="theme-btn btn-style-one" onClick={handleChangePassword}>
             Đổi mật khẩu
           </button>
         </div>
