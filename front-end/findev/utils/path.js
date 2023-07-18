d08be5e26e183ce8f369c7309b701f25f64436da
nhut19112001 <nhutsadow@gmail.com>
2023-07-18 20:05:07 +0700

    Update path.js

--- front-end/findev/utils/path.js
+++ front-end/findev/utils/path.js
@@ -2,3 +2,4 @@ export const localUrl = "http://localhost:8000/api";
 export const searchUrl = "http://20.242.247.247/api/server/jobs/";
 export const recommendUrl = "http://20.242.247.247/api/server/recommend-job";
 export const relevantUrl = "http://127.0.0.1:8001/recommendations";
+export const readCVUrl = "http://20.242.247.247/api/cv/read-cv";
