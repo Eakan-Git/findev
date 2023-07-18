0a0d512d7375d4b9e63cd5b188367faae0d116d1
nhut19112001 <nhutsadow@gmail.com>
2023-07-18 16:18:59 +0700

    UploadCV

--- front-end/findev/components/dashboard-pages/candidates-dashboard/cv-manager/index.jsx
+++ front-end/findev/components/dashboard-pages/candidates-dashboard/cv-manager/index.jsx
@@ -3,7 +3,7 @@ import LoginPopup from "../../../common/form/login/LoginPopup";
 import DashboardCandidatesSidebar from "../../../header/DashboardCandidatesSidebar";
 import BreadCrumb from "../../BreadCrumb";
 import CopyrightFooter from "../../CopyrightFooter";
-import CvUploader from "./components/CvUploader";
+import CvUploader from "./components/CvUploader"; // Import the CvUploader component here
 import DashboardCandidatesHeader from "../../../header/DashboardCandidatesHeader";
 import MenuToggler from "../../MenuToggler";
 import { useSelector } from "react-redux";
@@ -13,99 +13,92 @@ import { PDFDownloadLink } from "@react-pdf/renderer";
 import CVTemplate from "./components/CVTemplate";
 import { fetchProfile } from "./components/fetchProfile";
 import { useEffect, useState } from "react";
-const index = () => {
+
+const Index = () => {
   const { user } = useSelector((state) => state.user);
   const router = useRouter();
   // get user's profile
   const [profile, setProfile] = useState(null);
   const fetchUser = async () => {
-      const fetchedProfile = await fetchProfile(user.userAccount.id, user.token);
-      if (fetchedProfile.error === false) {
-          setProfile(fetchedProfile.data.user_profile);
-          // console.log("Profile:", fetchedProfile.data.user_profile);
-      }
-      else {
-          console.log("Failed to fetch profile data");
-      }
-  }
+    const fetchedProfile = await fetchProfile(user.userAccount.id, user.token);
+    if (fetchedProfile.error === false) {
+      setProfile(fetchedProfile.data.user_profile);
+    } else {
+      console.log("Failed to fetch profile data");
+    }
+  };
   useEffect(() => {
-      fetchUser();
-      // console.log(profile);
-  }
-  , []);
+    fetchUser();
+  }, []);
+
   if (!user) {
-    // show notification that user must login first
+    // show notification that the user must log in first
     alert("Bạn cần đăng nhập để xem thông tin cá nhân");
     router.push("/");
     return null;
   }
+
+  const [isUploaderVisible, setIsUploaderVisible] = useState(false);
+
+  const toggleUploader = () => {
+    setIsUploaderVisible(!isUploaderVisible);
+  };
+
+  const handleFileUpload = (files) => {
+    // Here you can write the code to handle the file upload process
+    // For example, you can use APIs to save the files, titles, and notes in the database.
+    console.log("Uploaded files:", files);
+  };
+
   return (
     <div className="page-wrapper dashboard">
       <span className="header-span"></span>
-      {/* <!-- Header Span for hight --> */}
 
       <LoginPopup />
-      {/* End Login Popup Modal */}
 
       <DashboardCandidatesHeader />
-      {/* End Header */}
 
       <MobileMenu />
-      {/* End MobileMenu */}
 
       <DashboardCandidatesSidebar />
-      {/* <!-- End Candidates Sidebar Menu --> */}
 
-      {/* <!-- Dashboard --> */}
       <section className="user-dashboard">
         <div className="dashboard-outer">
           <BreadCrumb title="Quản lý CV!" />
-          {/* breadCrumb */}
 
           <MenuToggler />
-          {/* Collapsible sidebar button */}
 
           <div className="row">
             <div className="col-lg-12">
-              {/* <!-- Ls widget --> */}
               <div className="cv-manager-widget ls-widget">
-              <div className="widget-title">
+                <div className="widget-title">
                   <h4>Danh sách CV của bạn</h4>
                   <div className="CV-button-wrapper">
-                    <button className="theme-btn btn-style-one">
+                    <button className="theme-btn btn-style-one" onClick={toggleUploader}>
                       Đăng tải CV&nbsp;<i className="la la-cloud-upload"></i>
                     </button>
                     &nbsp;&nbsp;
-                    <PDFDownloadLink document={<CVTemplate profile={profile}/>} fileName="CV.pdf">
+                    <PDFDownloadLink document={<CVTemplate profile={profile} />} fileName="CV.pdf">
                       <button className="theme-btn btn-style-one">
                         Tạo CV tự động&nbsp;<i className="la la-download"></i>
                       </button>
                     </PDFDownloadLink>
                   </div>
                 </div>
-                {/* End widget-title */}
+                {isUploaderVisible && <CvUploader user={user} onFileUpload={handleFileUpload} />}
+                &nbsp;&nbsp;
                 <div className="widget-content">
                   <CVListingsTable user={user} />
-                  {/* <CvUploader /> */}
                 </div>
-                {/* End widget-content */}
               </div>
-              {/* End Ls widget */}
             </div>
-            {/* End .col */}
           </div>
-          {/* End .row */}
         </div>
-        {/* End dashboard-outer */}
       </section>
-      {/* <!-- End Dashboard --> */}
 
       <CopyrightFooter />
-      {/* <CVTemplate profile={profile}/> */}
-      {/* <!-- End Copyright --> */}
     </div>
-    // End page-wrapper
   );
 };
 
-export default index;
+export default Index;
