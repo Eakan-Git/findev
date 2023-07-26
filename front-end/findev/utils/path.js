11c31831a0da85fe00c830a37da109cb761a163f
Eakan <58219087+Eakan-Git@users.noreply.github.com>
2023-07-27 00:08:08 +0700

    Update path.js

--- front-end/findev/utils/path.js
+++ front-end/findev/utils/path.js
@@ -2,7 +2,7 @@ export const localUrl = "https://job-recommendation-app-2lw5b.ondigitalocean.app
 // export const localUrl = "http://localhost:8000/api";
 export const searchUrl = "http://20.240.38.246/api/server/jobs";
 export const recommendUrl = "http://20.240.38.246/api/server/recommend-job";
-export const readCVUrl = "http://20.240.38.246//api/cv/read-cv";
+export const readCVUrl = "http://20.240.38.246/api/cv/read-cv";
 export const relatedUrl = "http://20.240.38.246/api/server/recommendations/?title=";
 
 // export const localUrl = process.env.localUrl;
