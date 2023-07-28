bcc743e25c83d600f66a9a9bc1d6a62dca9fa802
Eakan <58219087+Eakan-Git@users.noreply.github.com>
2023-07-29 00:59:27 +0700

    Update path.js

--- front-end/findev/utils/path.js
+++ front-end/findev/utils/path.js
@@ -1,7 +1,7 @@
 export const localUrl = "https://jrc-app-mftzj.ondigitalocean.app/api";
 // export const localUrl = "http://localhost:8000/api";
 export const searchUrl = "https://20.91.169.93.nip.io/api/server/jobs";
-export const recommendUrl = "https://20.91.169.93.nip.io/api/server/recommend-job";
+export const recommendUrl = "https://20.242.247.247.nip.io/api/job-recommend/recommend-job";
 export const readCVUrl = "https://20.91.169.93.nip.io/api/cv/read-cv";
 export const relatedUrl = "https://20.91.169.93.nip.io/api/server/title?title=";
 // export const localUrl = process.env.localUrl;
