05778c448b2b82ebcba46442f1de436d53eb5db1
Eakan <58219087+Eakan-Git@users.noreply.github.com>
2023-07-31 19:50:24 +0700

    update path

--- front-end/findev/utils/path.js
+++ front-end/findev/utils/path.js
@@ -1,7 +1,8 @@
 export const localUrl = "https://jrc-app-mftzj.ondigitalocean.app/api";
 // export const localUrl = "http://localhost:8000/api";
 export const searchUrl = "https://20.242.247.247.nip.io/api/search/jobs";
-export const recommendUrl = "https://20.242.247.247.nip.io/api/job-recommend/recommend-job";
+// export const recommendUrl = "https://20.242.247.247.nip.io/api/job-recommend/recommend-job";
+export const recommendUrl = "https://20.242.247.247.nip.io/api/job-recommend/recommend/?user_id=";
 export const readCVUrl = "https://20.242.247.247.nip.io/api/cv/read-cv";
 export const relatedUrl = "https://20.242.247.247.nip.io/api/search/title?title=";
 export const rateUrl = "https://20.242.247.247.nip.io/api/rate/update_data/";
