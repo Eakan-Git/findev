01cb1ba20625960928a3f43a0e8d74e0b4d69e88
nhut19112001 <nhutsadow@gmail.com>
2023-08-03 21:59:09 +0700

    Update path.js

--- front-end/findev/utils/path.js
+++ front-end/findev/utils/path.js
@@ -4,11 +4,12 @@ export const localUrl = "https://jrc-app-mftzj.ondigitalocean.app/api";
 export const searchUrl = "https://jrc-app-mftzj.ondigitalocean.app/api/jobs";
 // export const searchUrl = "http://localhost:8000/api/jobs";
 // export const recommendUrl = "https://20.242.247.247.nip.io/api/job-recommend/recommend-job";
-export const recommendUrl = "https://20.242.247.247.nip.io/api/job-recommend/recommend/?user_id=";
-export const readCVUrl = "https://20.242.247.247.nip.io/api/cv/read-cv";
-export const relatedUrl = "https://20.242.247.247.nip.io/api/search/title?title=";
-export const rateUrl = "https://20.242.247.247.nip.io/api/rate/update_data/";
-export const getRateUrl = "https://20.242.247.247.nip.io/api/rate/get_rate/";
+export const recommendUrl = "https://20.204.216.47.nip.io/api/job-recommend/recommend/?user_id=";
+export const readCVUrl = "https://20.204.216.47.nip.io/api/cv/read-cv";
+export const relatedUrl = "https://20.204.216.47.nip.io/api/search/title?title=";
+
+export const rateUrl = "https://20.204.216.47.nip.io/api/rate/update_data/";
+export const getRateUrl = "https://20.204.216.47.nip.io/api/rate/get_rate/";
 // export const localUrl = process.env.localUrl;
 // export const searchUrl = process.env.searchUrl;
 // export const recommendUrl = process.env.recommendUrl;
