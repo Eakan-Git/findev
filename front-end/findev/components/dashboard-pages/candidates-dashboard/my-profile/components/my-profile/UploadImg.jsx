8d2292ef18a245736e35175bbb024e56d1bb4aec
Eakan <58219087+Eakan-Git@users.noreply.github.com>
2023-08-04 22:19:06 +0700

    update detect face warning

--- front-end/findev/components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/UploadImg.jsx
+++ front-end/findev/components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/UploadImg.jsx
@@ -1,9 +1,11 @@
-import { localUrl } from "/utils/path";
+import { localUrl, checkImgUrl } from "/utils/path";
 
 // Pass 'user' as a parameter to the function
 export async function UploadImg (file, user) {
   const formData = new FormData();
   formData.append("avatar", file);
+  const checkImgData = new FormData();
+  checkImgData.append("image", file);
   try {
     const response = await fetch(
       `${localUrl}/user-profiles/avatar/${user.user.userAccount.id}`,
@@ -15,11 +17,25 @@ export async function UploadImg (file, user) {
         body: formData,
       }
     );
+    const checkImg = await fetch(
+      `${checkImgUrl}`,
+      {
+        method: "POST",
+        body: checkImgData,
+      }
+    );
+    let checkImgRes = await checkImg.json();
+    // console.log("checkImg", checkImgRes);
     // console.log("response", response);
-    if(!response.ok){
-      alert("Lỗi upload ảnh");
+    if(response.ok){
+      if(!checkImgRes?.message){
+        alert("Bạn nên sử dụng hình ảnh có khuôn mặt rõ ràng để tăng khả năng tìm kiếm việc làm");
+      }
+      window.location.reload();
     }
-    else window.location.reload();
+    else{
+      alert("Có lỗi xảy ra. Vui lòng thử lại");
+    }
   } catch (err) {
     console.log(err);
   }
