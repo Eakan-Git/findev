abf1cccb4aecb4fa51f7bbc0df86556f7fb894a8
PhamAnhTuan084 <tuansoi19127084@gmail.com>
2023-08-06 08:49:43 +0700

    update code + code deploy

--- front-end/findev/app/actions/userActions.js
+++
@@ -1,4 +0,0 @@
-export const USER_LOGOUT = 'USER_LOGOUT';
-export const logoutUser = () => {
-  return { type: USER_LOGOUT };
-};
--- front-end/findev/utils/path.js
+++
@@ -1,3 +0,0 @@
-export const localUrl = "http://localhost:8000/api";
-export const searchUrl = "http://localhost:8001/jobs/";
-export const recommendUrl = "http://localhost:8001/recommend_jobs/";
